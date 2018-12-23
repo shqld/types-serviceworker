df0e4b80194972a849ae74ced3e446dc1378bee3
Sho Miyamoto <shqld8@gmail.com>
2018-12-23 13:47:39 +0900

    add: index.d.ts that imports src/webworker.d.ts with `declare var`

---
+++ index.d.ts
@@ -0,0 +1,6 @@
+/// <reference no-default-lib="true" />
+/// <reference lib="es2015" />
+/// <reference lib="es2017.string" />
+/// <reference path="./src/webworker.d.ts" />
+
+declare var self: ServiceWorkerGlobalScope;
